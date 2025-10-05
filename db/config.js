22fe86bfe86ee49410b04c19f30fdd58698cd433
deeputkarsh007 <deeputkarsh007@gmail.com>
2025-10-05 19:09:35 +0530

    shifting DB to atlas

--- db/config.js
+++ db/config.js
@@ -1,2 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/e-commerce");
+mongoose.connect(
+  "mongodb+srv://muskanshr7970_db_user:jDZKkqtmKDaXg3ee@cluster0.fbehgyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
